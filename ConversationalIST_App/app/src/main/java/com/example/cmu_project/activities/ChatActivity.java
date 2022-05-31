ef6445c144ea186dee65a78ac001593a0868a3ce
ist1102146 <sebastiao.sotto.mayor@tecnico.ulisboa.pt>
2022-05-31 23:07:30 +0100

    feat: implement photo and geolocation media type chat support

--- ConversationalIST_App/app/src/main/java/com/example/cmu_project/activities/ChatActivity.java
+++ ConversationalIST_App/app/src/main/java/com/example/cmu_project/activities/ChatActivity.java
@@ -1,11 +1,17 @@
 package com.example.cmu_project.activities;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -17,8 +23,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.adevinta.leku.LocationPickerActivity;
 import com.example.cmu_project.R;
 
+import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.lang.ref.WeakReference;
@@ -39,6 +47,8 @@ import io.grpc.examples.helloworld.HelloRequest;
 public class ChatActivity extends AppCompatActivity {
 
     private static final Logger logger = Logger.getLogger(ChatActivity.class.getName());
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_LOCATION_PICKER = 2;
 
     private Button sendButton;
     private EditText hostEdit;
@@ -138,6 +148,104 @@ public class ChatActivity extends AppCompatActivity {
 
             sendButton.setEnabled(true);
         }
+    }
+
+    public void showMap(View view) {
+        Intent locationPickerIntent = new LocationPickerActivity.Builder()
+                .withLocation(41.4036299, 2.1743558)
+                .withGeolocApiKey("AIzaSyBVHoyrgJOlu_Zla_PLsWFXb8XMhVzCxDU")
+                .withSearchZone("en_EN")
+                //.withSearchZone(SearchZoneRect(LatLng(26.525467, -18.910366), LatLng(43.906271, 5.394197)))
+                .withDefaultLocaleSearchZone()
+                .shouldReturnOkOnBackPressed()
+                .withStreetHidden()
+                .withCityHidden()
+                .withZipCodeHidden()
+                .withSatelliteViewHidden()
+                //.withGooglePlacesEnabled()
+                .withGoogleTimeZoneEnabled()
+                .withVoiceSearchHidden()
+                .withUnnamedRoadHidden()
+                //.withSearchBarHidden()
+                .build(getApplicationContext());
+
+        try {
+            startActivityForResult(locationPickerIntent, REQUEST_LOCATION_PICKER);
+        } catch (ActivityNotFoundException e) {
+            logger.warning(e.getMessage());
+        }
+
+    }
+
+    private void sendGeolocation(String geolocation) {
+        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+                .hideSoftInputFromWindow(hostEdit.getWindowToken(), 0);
+        sendButton.setEnabled(false);
+
+        new GrpcTask(this, messageRecycler)
+                .execute(
+                        hostEdit.getText().toString(),
+                        geolocation,
+                        portEdit.getText().toString());
+    }
+
+    public void showCamera(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            logger.warning(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            sendPhoto(imageBitmap);
+        } else if (requestCode == REQUEST_LOCATION_PICKER && resultCode == RESULT_OK) {
+            Double latitude = data.getDoubleExtra("", 0.0);
+            Log.d("LATITUDE****", latitude.toString());
+            Double longitude = data.getDoubleExtra("", 0.0);
+            Log.d("LONGITUDE****", longitude.toString());
+            /*
+            String address = data.getStringExtra("");
+            Log.d("ADDRESS****", address != null ? address : "");
+            String postalcode = data.getStringExtra("");
+            Log.d("POSTALCODE****", postalcode != null ? postalcode : "");
+            String timeZoneId = data.getStringExtra("");
+            Log.d("TIME ZONE ID****", timeZoneId != null ? timeZoneId : "");
+            String timeZoneDisplayName = data.getStringExtra("");
+            Log.d("TIME ZONE NAME****", timeZoneDisplayName != null ? timeZoneDisplayName : "");
+             */
+            String geolocation = latitude + "/" + longitude;
+            sendGeolocation(geolocation);
+        }
+        if (resultCode == Activity.RESULT_CANCELED) {
+            Log.d("RESULT****", "CANCELLED");
+        }
+    }
+
+    private void sendPhoto(Bitmap imageBitmap) {
+        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+                .hideSoftInputFromWindow(hostEdit.getWindowToken(), 0);
+        sendButton.setEnabled(false);
+
+        new GrpcTask(this, messageRecycler)
+                .execute(
+                        hostEdit.getText().toString(),
+                        bitMapToString(imageBitmap),
+                        portEdit.getText().toString());
+    }
+
+    private String bitMapToString(Bitmap bitmap){
+        ByteArrayOutputStream baos=new  ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
+        byte [] b=baos.toByteArray();
+        String temp= Base64.encodeToString(b, Base64.DEFAULT);
+        return temp;
     }
 
 }
